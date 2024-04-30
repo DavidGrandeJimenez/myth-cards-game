21a631be4ee44796a42bfbe59acc650f00408d5e
David GRANDE <davidgrandewebs@gmail.com>
2024-04-30 10:44:56 +0200

    Commit 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -265,17 +265,17 @@ const App = () => {
 
   //Constantes de los estilos de los Typography
   const sxFotito = {
-    width:{xs: '90%'},
-    fontSize: { xs: '1.8rem', sm: '2.5rem', md: '2.7rem', lg: '2.9rem' },
+    width:{xs: '90%', lg:'120%'},
+    fontSize: { xs: '1.5rem', sm: '2.5rem', md: '2.7rem', lg: '2.3rem' },
     fontFamily: 'Times New Roman, serif', 
     fontStyle: 'italic',
     fontWeight: 'bold',
-    marginTop: { xs: '5vh', sm: '-10vh', md: '-2vh', lg: '-20vh' },
+    marginTop: { xs: '-7vh', sm: '-10vh', md: '-2vh', lg: '-20vh' },
     lineHeight: { xs: '5vh', sm: '6vh', md: '7vh', lg: '8vh' }
   };
 
   const sxFotito2 = {
-    fontSize: { xs: '1.6rem', sm: '1.8rem', md: '2.5rem', lg: '2.9rem' },
+    fontSize: { xs: '1.4rem', sm: '1.8rem', md: '2.5rem', lg: '2.3rem' },
     fontFamily: 'Times New Roman, serif',
     fontStyle: 'italic',
     fontWeight: 'bold',
@@ -284,12 +284,11 @@ const App = () => {
   };
 
   const letrasResultado = {
-    fontSize: { xs: '1.6rem', sm: '2.8rem', md: '4rem', lg: '2.9rem' },
+    fontSize: { xs: '1.2rem', sm: '2.8rem', md: '4rem', lg: '2.9rem' },
     fontFamily: 'Times New Roman',
     fontStyle: 'italic',
     fontWeight: 'bold',
     marginTop: { xs: '0vh', sm: '0vh', md: '0vh', lg: '0vh' },
-
     lineHeight: { xs: '4vh', sm: '5vh', md: '5vh', lg: '8vh' }
   };
 
